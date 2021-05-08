bc97a118d67faf4d929e978ed3148923494ffa0a
gunjan <singhalgunjan5@gmail.com>
2021-05-08 18:40:14 +0530

    Initial Commit

---
+++ practice c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  practice c
+//
+//  Created by apple on 08/05/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
